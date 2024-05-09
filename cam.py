fe9198a3ebc65c4173ed2ca70c9fab8cec7cf5bf
raevillena <rhaey1995@gmail.com>
2024-05-09 09:50:39 +0800

    add

--- cam.py
+++ cam.py
@@ -5,4 +5,4 @@ picam2.start(show_preview=True)
 picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
 picam2.capture_file("test6.jpg")
 picam2.stop_preview()
-picam2.stop()
+picam2.stop()git 
