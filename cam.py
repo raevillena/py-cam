bb1e7ade20a15f0216f18e43b1d0a2463438eb46
raevillena <rhaey1995@gmail.com>
2024-05-09 09:57:05 +0800

    add

--- cam.py
+++ cam.py
@@ -1,8 +1,13 @@
 from picamera2 import Picamera2
 from libcamera import controls
+from os import walk
+
 picam2 = Picamera2()
 picam2.start(show_preview=True)
 picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
 picam2.capture_file("test6.jpg")
 picam2.stop_preview()
-picam2.stop()git 
+picam2.stop()
+
+filenames = next(walk(mypath), (None, None, []))[2]  # [] if no file
+print(filenames)
