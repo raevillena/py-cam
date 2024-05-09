3247dde25e2a71f1655b592ff115cce176661576
raevillena <rhaey1995@gmail.com>
2024-05-09 10:06:43 +0800

    adding folders

--- cam.py
+++ cam.py
@@ -1,6 +1,6 @@
 from picamera2 import Picamera2
 from libcamera import controls
-from os import walk
+
 
 picam2 = Picamera2()
 picam2.start(show_preview=True)
@@ -9,5 +9,18 @@ picam2.capture_file("test6.jpg")
 picam2.stop_preview()
 picam2.stop()
 
-filenames = next(walk(mypath), (None, None, []))[2]  # [] if no file
-print(filenames)
+
+import os
+
+# folder path
+dir_path = r'/home/admin/cam/overaged'
+
+# list to store files
+res = []
+
+# Iterate directory
+for path in os.listdir(dir_path):
+    # check if current path is a file
+    if os.path.isfile(os.path.join(dir_path, path)):
+        res.append(path)
+print(res)
